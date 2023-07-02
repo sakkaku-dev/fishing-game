c769c0a4ce6e2a568d1219f19b12b33b8f9b6232
myin <minyin142p@gmail.com>
2023-07-02 11:48:29 +0200

    animate sun/moon

--- src/world/sun.gd
+++ src/world/sun.gd
@@ -1,15 +1,3 @@
 extends Node2D
 
-@export var start := -1000
-@export var end := 1000
-
-@onready var sun := $Sun
-@onready var bg := $BG
-
 const SECONDS_IN_DAY = 86400
-
-func _physics_process(delta):
-	sun.move_local_y(0.1)
-	
-	if sun.global_position.y >= end:
-		sun.global_position.y = start
